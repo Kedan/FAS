7daca6aeba713ad38b186a5c93e43d1f69534ad8
Kedan <kedan@o2.pl>
2014-02-04 20:05:14 +0100

    Update invitation-email.php

--- views/invitation-email.php
+++ views/invitation-email.php
@@ -1,16 +1,9 @@
 <html>
 	<head>
-		<title>Bangama Invitation!</title>
+		<title>Hello</title>
 	</head>
 	<body>
-		<h1 style="color:#F00">This is only external mailer test! Do not use link below!</h1>
-		<h2>Hello <?php echo $username; ?></h2>
-		<p>
-			This is your invitation link for new bangama game!<br>
-			<a href="<?php echo $base_url;?>i:<?php echo $invitation_code; ?>"><?php echo $base_url;?>/i:<?php echo $invitation_code; ?></a>
-		</p>
-		<p>
-			Good luck!
-		</p>
+		<h1>FAS<h1>
+		<?php echo $data; ?>
 	</body>	
 </html>
